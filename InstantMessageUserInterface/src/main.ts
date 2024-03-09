8b4fc2d9da36b94bb9a9c53bf4922cf7fdf4c296
fengandzhy <fengandzhy@gmail.com>
2024-03-09 20:22:51 +1300

    delete the HelloWorld.vue

--- InstantMessageUserInterface/src/main.ts
+++ InstantMessageUserInterface/src/main.ts
@@ -1,8 +1,8 @@
-import {createApp} from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import {createApp} from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router';
 
-const elementApp = createApp(App)
-elementApp.use(router)
-elementApp.mount('#app')
+const elementApp = createApp(App);
+elementApp.use(router);
+elementApp.mount('#app');
