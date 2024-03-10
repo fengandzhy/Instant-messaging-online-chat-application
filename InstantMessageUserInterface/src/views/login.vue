192e3f8bcfe44271114b400b484101fdfa25e7f6
fengandzhy <fengandzhy@gmail.com>
2024-03-10 20:58:46 +1300

    update login.vue

--- InstantMessageUserInterface/src/views/login.vue
+++ InstantMessageUserInterface/src/views/login.vue
@@ -1,16 +1,31 @@
 <template>
-  <div>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
+  <div class="login">
+    <div class="container">
+      <el-row>
+        <el-col :span="15">
+          <div class="grid-content ep-bg-purple" />
+        </el-col>
+        <el-col :span="9">
+          <div class="grid-content ep-bg-purple-light" />
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-name: "login"
+  name: "login"
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  background: #cecfff;
+}
 </style>
